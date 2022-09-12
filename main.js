515b770c72ebfe266cff89b8c96a286f50a54fd0
tubi55 <tubi55@nate.com>
2022-09-12 19:01:00 +0900

    txt 글자 분리하기

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ const main = document.querySelector('main')
 const btns = main.querySelectorAll('ul li');
 const boxs = main.querySelectorAll('article');
 
+boxs.forEach((box, idx) => {
+  const h1 = box.querySelector('h1');
+  const txt = h1.innerText;
+  let tags = '';
+
+  for (el of txt) tags += `<span>${el}</span>`;
+  h1.innerHTML = tags;
+})
+
 btns.forEach((btn, idx) => {
   btn.addEventListener('click', e => {
     e.preventDefault();
