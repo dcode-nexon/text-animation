5ecc17a20e6e96b7bb3d3db30123b2b672d88645
tubi55 <tubi55@nate.com>
2022-09-12 18:32:32 +0900

    배경색 변경

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const btns = document.querySelectorAll('ul li');
+const main = document.querySelector('main')
+const btns = main.querySelectorAll('ul li');
 
-btns.forEach(btn => {
+btns.forEach((btn, idx) => {
   btn.addEventListener('click', e => {
     e.preventDefault();
+
+    main.className = `bg${idx + 1}`;
 
     for (el of btns) el.classList.remove('on')
     btn.classList.add('on');
