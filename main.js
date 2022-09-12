668a0e75b0ea82948cf59c89bac1b09166ff318f
tubi55 <tubi55@nate.com>
2022-09-12 18:54:07 +0900

    슬로건 텍스트 스타일링

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const main = document.querySelector('main')
 const btns = main.querySelectorAll('ul li');
+const boxs = main.querySelectorAll('article');
 
 btns.forEach((btn, idx) => {
   btn.addEventListener('click', e => {
@@ -7,7 +8,10 @@ btns.forEach((btn, idx) => {
 
     main.className = `bg${idx + 1}`;
 
-    for (el of btns) el.classList.remove('on')
-    btn.classList.add('on');
+    for (el of btns) el.classList.remove('on');
+    for (el of boxs) el.classList.remove('on');
+    btns[idx].classList.add('on');
+    boxs[idx].classList.add('on');
+
   })
 })
